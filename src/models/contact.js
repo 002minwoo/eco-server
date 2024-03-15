ce20e6fa207725d3ad393d79a2425069bdddcdf7
fptcsm <gianglamweb@gmail.com>
2024-03-16 02:58:15 +0700

    commit

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,16 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const contact = sequelize.define('contact', {
-    name: DataTypes.STRING,
     date_send: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    content: DataTypes.STRING,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    code_phone: DataTypes.STRING,
+    condition_delivery: DataTypes.STRING,
+    departure: DataTypes.STRING,
+    destination: DataTypes.STRING,
     email: DataTypes.STRING,
-    status: DataTypes.STRING,
-    reply_text: DataTypes.STRING,
-    user_reply: DataTypes.INTEGER,
-    type: DataTypes.STRING,
-    product: DataTypes.STRING
+    hscode: DataTypes.STRING,
+    item: DataTypes.STRING,
+    nation: DataTypes.STRING,
+    note: DataTypes.STRING,
+    owner: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    price: DataTypes.STRING,
+    time_end: DataTypes.STRING,
+    time_start: DataTypes.STRING,
+    weight: DataTypes.STRING,
+    company: DataTypes.STRING,
+    tax_code: DataTypes.STRING,
+    tax_place: DataTypes.STRING,
+    type: DataTypes.INTEGER,
+    unit: DataTypes.STRING,
+    file: DataTypes.STRING  
   }, {});
   return contact;
 };
